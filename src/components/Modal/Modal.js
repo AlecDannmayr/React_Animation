e8dcc1f9252e7715b114e94f58a7ab20ce685e71
AlecDannmayr <alec.dannmayr@gmail.com>
2023-05-15 14:56:52 +1000

    issue resolved

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Transition } from "react-transition-group";
+import { CSSTransition } from "react-transition-group/";
 import "./Modal.css";
 
 const animationTiming = {
@@ -10,7 +10,7 @@ const animationTiming = {
 const modal = (props) => {
 
   return (
-    <Transition in={props.show} timeout={animationTiming} mountOnEnter unmountOnExit>
+    <CSSTransition in={props.show} timeout={animationTiming} mountOnEnter unmountOnExit classNames={"fade-slide"}>
       {state => {
           const cssClasses = [ "Modal", state === "entering" ? "ModalOpen" : state === "exiting" ? "ModalClosed" : null]
         return(
@@ -22,7 +22,7 @@ const modal = (props) => {
           </div>
         );
       }}
-    </Transition>
+    </CSSTransition>
   );
 };
 
